c8d74f1e5a645c06a3c95e203f20644a32491165
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-03-15 20:54:20 +0530

    Dashboard fix

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -3,11 +3,11 @@ import { balanceAtom } from '../store/atoms/user'
 
 export default function Balance() {
     const { balance } = useRecoilValue(balanceAtom);
-    // console.log(balance);
+    const twoDecimal = parseFloat(balance).toFixed(2);
     return (
         <div className='bg-slate-800'>
             <div className=' w-full lg:max-w-7xl mx-auto px-4 md:px-0 py-8 text-white '>
-                <span className=' text-lg font-bold'>Your Balance {balance}</span>
+                <span className=' text-lg font-bold'>Your Balance {twoDecimal}</span>
             </div>
         </div>
     )
