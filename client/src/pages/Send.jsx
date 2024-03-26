445e6e35e18f0d9c7f3f01fa556a7aa3e31923cc
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-03-27 00:44:13 +0530

    back button added in transaction send page

--- client/src/pages/Send.jsx
+++ client/src/pages/Send.jsx
@@ -1,17 +1,23 @@
 import InputBox from '../components/InputBox';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { amountAtom } from '../store/atoms/user';
 import useTransfer from '../hooks/Transfer';
+import { IoArrowBack } from "react-icons/io5";
 
 export default function Send() {
     const [amount, setAmount] = useRecoilState(amountAtom);
     const [searchParams] = useSearchParams();
     const id = searchParams.get("id");
     const name = searchParams.get("name");
-    const handleTransfer = useTransfer({amount, id});
+    const handleTransfer = useTransfer({ amount, id });
     return (
         <>
+            <div className=' absolute text-white p-5 text-2xl'>
+                <Link to="/dashboard">
+                    <IoArrowBack />
+                </Link>
+            </div>
             <div className=' min-h-screen w-full bg-zinc-900 flex items-center'>
                 <section className=' mx-auto text-white flex justify-center items-center py-8 px-6 sm:px-8 md:px-10 bg-zinc-800 rounded-lg w-[90%] sm:w-[95%] md:w-[50%] xl:w-96'>
                     <div className=' flex items-center flex-col w-full'>
